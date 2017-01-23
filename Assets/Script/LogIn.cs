e2aa5092bcae6825b4a5dcb175cbcb6e1206c427
Saeki <@saeki.com>
2017-01-23 12:17:45 +0900

    ログイン画面 あべ

--- Assets/Script/LogIn.cs
+++ Assets/Script/LogIn.cs
@@ -1,13 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LogIn : MonoBehaviour {
+public class LogIn : MonoBehaviour
+{
 
     public GameManager mManager;
+    public string id;
 
     void Login(string id)
     {
         mManager.mID = id;
         Instantiate(mManager);
+    }
+
+    //プレイヤー１か２判別
+    public void Plyer1()
+    {
+        id = "Plyer1";
+    }
+
+    public void Plyer2()
+    {
+        id = "Plyer2";
+    }
+
+
+    void stertLogin()
+    {
+        
+
+    }
+
+    void update()
+    {//確認
+       print(id);
     }
 }
