6985feb481791fe733f30b0840f8acd24aebffba
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-26 11:14:16 +0900

    （仮実装）選択した役に応じてボードの向きを変えるようにした

---
+++ Assets/Script/MainGame/MainLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainLoader : MonoBehaviour
+{
+    enum Job
+    {
+        Emperor,
+        Slave
+    }
+
+    [SerializeField, Tooltip("説明文")]
+    Job job;
+
+    [SerializeField]
+    GameObject board;
+    
+    void Start()
+    {
+        Vector3 scale = board.transform.localScale;
+
+        switch(job)
+        {
+            case Job.Emperor: scale.y =  1; break;
+            case Job.Slave:   scale.y = -1; break;
+        }
+
+        board.transform.localScale = scale;
+    }
+}
