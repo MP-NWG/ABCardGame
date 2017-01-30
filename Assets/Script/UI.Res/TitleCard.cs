d83720d967ade9807d6d78fa1f8f63b89072d2a0
tunomizu <tumikihurai46@gmail.com>
2017-01-30 15:34:54 +0900

    ログイン画面　変更　アベベ

--- Assets/Script/UI.Res/TitleCard.cs
+++ Assets/Script/UI.Res/TitleCard.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class TitleCard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public float _Speed  = 0;
+    private float y = 0;
+    void Start () {
+        y = transform.localPosition.y;
+    }
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        transform.Rotate(new Vector3(0, 2, 0));
+        y += -_Speed * Time.deltaTime;
+        if (transform.localPosition.y <= -500)
+        {
+            y = 0;
+        }
+        transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+
+    }
 }
